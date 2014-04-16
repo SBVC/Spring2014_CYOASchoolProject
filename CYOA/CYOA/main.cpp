536c89cb81434b38fb4692b2d811430c37aed0ca
ZacharyHebert <alwayskeepcontents@yahoo.com>
2014-04-15 17:34:09 -0700

    Created Visual Studio Project

---
+++ CYOA/CYOA/main.cpp
@@ -0,0 +1,15 @@
+/*
+Choose Your Own Adventure
+School Project CS110-70 Spring 2014
+By Nathaniel Orimsomething?, Zachary Hebert, and Juan Rameriez?
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	system("PAUSE");
+
+	return 0;
+}

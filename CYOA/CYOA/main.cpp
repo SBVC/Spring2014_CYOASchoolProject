89e1a13c0e1c1b7b3632f065ea3fcea7a5099a48
ZacharyHebert <alwayskeepcontents@yahoo.com>
2014-04-22 13:55:32 -0700

    Added switch statement

--- CYOA/CYOA/main.cpp
+++ CYOA/CYOA/main.cpp
@@ -12,12 +12,29 @@ int main(){
 	system("PAUSE");
 
 	cout << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" << endl << endl;
-	cout << "1. bbbb\n2. ccccccc\n3. dddddd\n4. eeeeeeee";
+	cout << "1. bbbb\n2. ccccccc\n3. dddddd\n4. eeeeeeee\n5. Quit";
 
 	int userChoice;
 	cin >> userChoice;
 
-
+	switch (userChoice){
+	case 1:
+		entryB();
+		break;
+	case 2:
+		entryC();
+		break;
+	case 3:
+		entryD();
+		break;
+	case 4:
+		entryE();
+		break;
+	case 5:
+		break;
+	default:
+		cout << "Recongnized inputs: 1, 2, 3, 4, 5.\n";
+	}
 
 	return 0;
 }

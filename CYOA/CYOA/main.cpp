15da7f14d7655fc021ce6a252c33d5157e15b3af
Nathan142005 <nathan142005@gmail.com>
2014-04-22 14:06:27 -0700

    Nate's Commit
    This project is making me fail my commitments... guess you can only
    commit to one thing at a time...see what I did there?

--- CYOA/CYOA/main.cpp
+++ CYOA/CYOA/main.cpp
@@ -11,12 +11,16 @@ using namespace std;
 int main(){
 	system("PAUSE");
 
-	cout << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" << endl << endl;
-	cout << "1. bbbb\n2. ccccccc\n3. dddddd\n4. eeeeeeee";
+	cout << "Welcome to Choose Your Own Adventure... Choose wisely...you could be killed at any moment" << endl << endl;
+	cout << "Press a to begin...or any letter really, whichever letter is your favorite, just pick one...come on...any day now\n";
+	cout << "1. a\n2. b\n3. c\n4. d";
+	cout << "Press 0 to quit though...just saying...you don't HAVE to play...seriously if you don't want to...don't\n";
 
 	int userChoice;
 	cin >> userChoice;
-
+	switch (userChoice)
+		case 0: break 
+		
 
 
 	return 0;

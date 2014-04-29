a2d008d86e82a5ef870ef1dbd1c1ceb055f35c0e
ZacharyHebert <alwayskeepcontents@yahoo.com>
2014-04-28 22:15:54 -0700

    Fixed bugs

--- CYOA/CYOA/main.cpp
+++ CYOA/CYOA/main.cpp
@@ -1,39 +1,45 @@
 /*
 Choose Your Own Adventure
 School Project CS110-70 Spring 2014
-By Nathaniel Orimlade, Zachary Hebert, and Juan Rameriez?
+By Nathaniel Orimlade, Zachary Hebert, and Juan Rameriez
 */
 
 #include <iostream>
 
 using namespace std;
 
+//function prototypes
 int entryA();
 int entryB();
 int entryC();
 int entryD();
 int entryE();
 
-int main(){
-	entryA();
-	cout << "Welcome to Choose Your Own Adventure... Choose wisely...you could be killed at any moment" << endl << endl;
-	cout << "Press a to begin...or any letter really, whichever letter is your favorite, just pick one...come on...any day now\n";
-	cout << "1. a\n2. b\n3. c\n4. d";
-	cout << "Press 0 to quit though...just saying...you don't HAVE to play...seriously if you don't want to...don't\n";
+//global variables
+int userChoice;
 
-	int userChoice;
+int main(){
+	cout << "Welcome to Choose Your Own Adventure... Choose wisely...you could be killed at any moment" << endl << endl;
+	cout << "Enter 1 to begin...or any number really*, whichever number is your favorite, just pick one...come on...any day now" << endl << endl;
+	cout << "*Enter 0 to quit though**...just saying...you don't HAVE to play...seriously if you don't want to...don't" << endl << endl;
+	cout << "**If you mess up and enter a letter, you loose." << endl << endl;
+
 	cin >> userChoice;
-	switch (userChoice)
-		case 0: break;
-	system("PAUSE");
+
+	switch (userChoice){
+		case 0:
+			break;
+		default:
+			entryA();
+	}
+
 	return 0;
 }
 
 int entryA(){
-	cout << "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" << endl << endl;
-	cout << "1. bbbb\n2. ccccccc\n3. dddddd\n4. eeeeeeee\n5. Quit\n";
+	cout << "\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" << endl << endl;
+	cout << "1. bbbb\n2. ccccccc\n3. dddddd\n4. eeeeeeee\n0. Quit" << endl << endl;
 
-	int userChoice;
 	cin >> userChoice;
 
 	switch (userChoice){
@@ -49,27 +55,40 @@ int entryA(){
 	case 4:
 		entryE();
 		break;
-	case 5:
+	case 0:
 		break;
 	default:
-		cout << "Recongnized inputs: 1, 2, 3, 4, 5.\n";
+		cout << "Recongnized inputs: '1', '2', '3', '4', '0'." << endl << endl;
+		entryA();
 	}
 
 	return 0;
 }
 
 int entryB(){
+	cout << "\nbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" << endl << endl;
+	system("pause");
+
 	return 0;
 }
 
 int entryC(){
+	cout << "\nccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc" << endl << endl;
+	system("pause");
+
 	return 0;
 }
 
 int entryD(){
+	cout << "\ndddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" << endl << endl;
+	system("pause");
+
 	return 0;
 }
 
 int entryE(){
+	cout << "\neeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" << endl << endl;
+	system("pause");
+
 	return 0;
 }
